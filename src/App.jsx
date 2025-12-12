9af8661d35da0c37828324aab591b729d756b4d0
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2025-12-12 15:26:56 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+const Avatar = () => {
+   return (
+      <img 
+         src="https://i.imgur.com/1bX5QH6.jpg" 
+         alt="Lin Lanying" 
+         className="avatar"
+         width={100}
+         height={100}
+      />
+   )
+}
+
+const App = () => {
+  return (
+    <Avatar />
+  )
+}
+export default App
